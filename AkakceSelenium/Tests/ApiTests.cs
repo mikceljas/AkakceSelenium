516575fd6fd6e8492f7a4286e04ec1d628b0995e
mikceljas <mikceljas@DESKTOP-H170JMN>
2025-01-31 18:31:29 +0300

    Add project files.

---
+++ AkakceSelenium/Tests/ApiTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Xunit;
+using FluentAssertions;
+
+
+namespace AkakceSelenium.Tests
+{
+    public class ApiTests
+    {
+        private readonly RestClient _client;
+        private const string BASE_URL = "https://jsonplaceholder.typicode.com/posts";
+
+        public ApiTests()
+        {
+            _client = new RestClient(BASE_URL);
+        }
+
+        [Fact]
+        public async Task StatusCode_Should_Be_200()
+        {
+            var request = new RestRequest("", Method.Get);
+            var response = await _client.ExecuteAsync(request);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task JsonResponse_Should_Have_Required_Fields()
+        {
+            var request = new RestRequest("", Method.Get);
+            var response = await _client.ExecuteAsync<List<Post>>(request);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            response.Data.Should().NotBeNullOrEmpty();
+            response.Data[0].Should().NotBeNull();
+            response.Data[0].UserId.Should().BeGreaterThan(0);
+            response.Data[0].Id.Should().BeGreaterThan(0);
+            response.Data[0].Title.Should().NotBeNullOrEmpty();
+            response.Data[0].Body.Should().NotBeNullOrEmpty();
+        }
+
+        [Fact]
+        public async Task Post_With_Id_1_Should_Have_Title()
+        {
+            var request = new RestRequest("/1", Method.Get);
+            var response = await _client.ExecuteAsync<Post>(request);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            response.Data.Should().NotBeNull();
+            response.Data.Id.Should().Be(1);
+            response.Data.Title.Should().NotBeNullOrEmpty();
+        }
+
+        [Fact]
+        public async Task Response_Should_Have_At_Least_10_Posts()
+        {
+            var request = new RestRequest("", Method.Get);
+            var response = await _client.ExecuteAsync<List<Post>>(request);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            response.Data.Count.Should().BeGreaterThanOrEqualTo(10);
+        }
+
+        [Fact]
+        public async Task All_UserIds_Should_Be_Positive()
+        {
+            var request = new RestRequest("", Method.Get);
+            var response = await _client.ExecuteAsync<List<Post>>(request);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            response.Data.Should().OnlyContain(p => p.UserId > 0);
+        }
+
+    }
+    public class Post
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+
+}
---
+++ AkakceSelenium/Tests/FollowItem.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AkakceSelenium.Tests
+{
+    internal class FollowItem
+    {
+        [Test]
+        public void Start()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.akakce.com/");
+            Thread.Sleep(3000);
+            driver.Manage().Window.Maximize();
+            //Login Page
+            var LoginButtonMain = driver.FindElement(By.XPath("//a[@href=\"/akakcem/giris/\"]"));
+            LoginButtonMain.Click();
+            var EmailText = driver.FindElement(By.Id("life"));
+            //Credentials
+            //Fill with mail address of an Akakce account
+            EmailText.SendKeys(""); 
+            var PasswordText = driver.FindElement(By.Id("lifp"));
+            //Fill with password of an Akakce account
+            PasswordText.SendKeys(""); 
+            var LoginButton = driver.FindElement(By.Id("lfb"));
+            LoginButton.Click();
+            
+            Thread.Sleep(2000);
+            //After search
+            driver.Navigate().GoToUrl("https://www.akakce.com/cep-telefonu/apple.html");
+            Thread.Sleep(2000);
+            //Add to followed items
+            var FollowItem = driver.FindElement(By.XPath("//span[@class=\"ufo_v8\"]"));
+            FollowItem.Click();
+
+            driver.Quit();
+
+            /*
+            //SearchBar.Click();
+            //Thread.Sleep(1000);
+            //SearchBar.SendKeys("iphone");
+            //var SearchBarButton = driver.FindElement(By.XPath("//i[normalize-space()='Ara']"));
+            //SearchBarButton.Click();
+            //var SearchBar = driver.FindElement(By.XPath("//input[@type=\"text\"]"));
+            //var LoginButtonMain = driver.FindElement(By.XPath("//div[@id='H_rl_v8']//a[@rel='nofollow'][contains(text(),'Giriş Yap')]"));
+            //var SearchBar = driver.FindElement(By.ClassName("frm_v8")); 
+            //var SearchBar = driver.FindElement(By.XPath("//input"));
+            //driver.FindElement(By.XPath("//input[@type=\"text\"]")).Click();
+            //var SearchBar = driver.FindElement(By.XPath("//form[@id=\"H_s_v8\"]"));
+            //var SearchBar2 = driver.FindElement(By.XPath("/ html[1] / body[1] / div[1] / header[1] / div[3] / form[1] / span[1] / input[1]"));
+            //IJavaScriptExecutor jse = (IJavaScriptExecutor)driver;
+            //jse.ExecuteScript("document.getElementByXPath('//input[@type=\"text\"]').setAttribute('Neyi ucuza almak istersin?','iphone')");
+             * */
+        }
+    }
+}
